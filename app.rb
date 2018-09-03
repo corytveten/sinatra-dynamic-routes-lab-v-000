03b01749c4eae01a4ea8e6c41e1aba1152df6b72
Cory Tveten <corytveten@gmail.com>
2018-09-03 13:56:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,11 +23,11 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @num1 = params[:number1].to_1
+    @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
     if params[:operation] == "add"
       @num1 + @num2
-    elsif params[:operation] == "subract"
+    elsif params[:operation] == "subtract"
       @num1 - @num2
     elsif params[:operation] == "multiply"
       @num1 * @num2
