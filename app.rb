035e8bf97240cec317515c59f1a627c4cfad3b6b
Cory Tveten <corytveten@gmail.com>
2018-09-03 13:54:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,4 +21,20 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
+
+  get '/:operation/:number1/:number2' do
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+    if params[:operation] == "add"
+      @num1 + @num2
+    elsif params[:operation] == "subract"
+      @num1 - @num2
+    elsif params[:operation] == "multiply"
+      @num1 * @num2
+    elsif params[:operation] == "divide"      
+      @num1 / @num2
+
+  end
 end
+
+
